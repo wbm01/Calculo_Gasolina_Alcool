8765d23f9d6ad86e57018cc772fa605cdc5d4fb1
Willian Marioti <willian.marioti@gmail.com>
2023-03-05 13:14:17 -0300

    Add project files.

---
+++ Gasolina_Alcool/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double gaso, alc, total;
+
+        Console.WriteLine("Informe o valor da gasolina: ");
+        gaso = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o valor do álcool: ");
+        alc = double.Parse(Console.ReadLine());
+
+        total = (alc/gaso)*100;
+
+        Console.WriteLine("Total: {0}", total.ToString("f2") + "%");
+
+        if(total <= 72)
+        {
+            Console.WriteLine("Abasteça com álcool.");
+        }
+        else
+        {
+            Console.WriteLine("Abasteça com gasolina.");
+        }
+    }
+}
